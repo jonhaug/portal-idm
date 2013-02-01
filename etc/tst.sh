2f471f24eceb50a4a2816d7486610ba70ef9b036
Jon Haugsand <jon.haugsand@gmail.com>
2013-02-01 16:40:30 +0100

    Trying pure node as script

--- etc/tst.sh
+++ etc/tst.sh
@@ -14,10 +14,6 @@ var cmd="$CMD"
 var strObject='$OBJECT'
 var parameters=$PAR_DEC
 
-//console.log("Command=" + cmd);
-//console.log("Object=" + object);
-//console.log("Parameters=" + parameters);
-
 function setDeep(path, object, value) {
     var first=path.shift();
     if (path.length == 0) {
@@ -37,19 +33,16 @@ function setDeep(path, object, value) {
 var object;
 try {
   object=JSON.parse(strObject);
-} catch (e) {
-  process.exit(code=1);
-}
+} catch (e) {  process.exit(code=1);}
 
 parameters.forEach(function (val, index, array) {
-//   console.log(index + ': ' + val);
    var i=val.indexOf('=');
    var keys=val.substring(0,i).split(/\./);
    var value=val.substring(i+1);
    setDeep(keys, object, value);
  });
 
-if (cmd='pp') {
+if (cmd=='pp') {
   console.log(JSON.stringify(object, null, 4))
 } else {
   console.log(JSON.stringify(object));
@@ -59,13 +52,8 @@ EOF
 }
 
 
-node-convert pp "{\"a\": \"jon\", \"b\":{ \"c\": \"value\" }}" x=anne b.d.e=Siri a=noj
+XX=$(node-convert json "{\"a\": \"jon\", \"b\":{ \"c\": \"value\" }}" x=anne b.d.e=Siri a=noj)
+echo "///$XX///"
 
-# (echo set -- abc def; cat <<EOF
-# echo hello
-# echo $1
-# echo $abc
-# EOF
-# ) | bash
+node-convert pp "{\"a\": \"jon\", \"b\":{ \"c\": \"value\" }}"
 
-# (echo set -- arg1 arg2; cat SCRIPT) 
